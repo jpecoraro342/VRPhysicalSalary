b81cbb64a837b746e5e98d5c1c2b33997f6a8340
jpecoraro342 <jpecoraro342@ufl.edu>
2016-03-23 14:52:15 -0400

    Money Dispenser is created and disperses pennies

---
+++ Assets/_Scripts/MoneyDispenser.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoneyDispenser : MonoBehaviour {
+
+	[SerializeField] float salaryAmount = 120000f;
+
+	[SerializeField] GameObject penny;
+	[SerializeField] GameObject nickel;
+	[SerializeField] GameObject dime;
+	[SerializeField] GameObject quarter;
+
+	// What is the maximum x and z that we can spawn the coins in around the central dispensing game object?
+	[SerializeField] float containingWidth;
+	[SerializeField] float containingLength;
+
+	[SerializeField] GameObject coinContainer;
+
+	private float pennyValue = 0.01f;
+	private float nickelValue = 0.05f;
+	private float dimeValue = 0.1f;
+	private float quarterValue = 0.25f;
+
+	private Coroutine pennyCoroutine;
+	private Coroutine nickelCoroutine;
+	private Coroutine dimeCoroutine;
+	private Coroutine quarterCoroutine;
+
+	// rate = coins per minute
+	private Dictionary<GameObject, float> coinRate;
+
+
+	// Use this for initialization
+	void Start () {
+		coinRate = new Dictionary<GameObject, float>() { 
+			{ penny , 0 },
+			{ nickel , 0 },
+			{ dime , 0 },
+			{ quarter , 0 }
+		};
+
+		StartCoroutine(runDemo());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private IEnumerator runDemo() {
+		startDispensing();
+
+		yield return new WaitForSeconds(5);
+
+		restartDispensingWithSalary(50000f);
+
+		yield return new WaitForSeconds(10);
+
+		stopDispensing();
+		removeAllCoins();
+	}
+
+	private IEnumerator spawnCoinAtRate(GameObject coin, float rate) {
+		if (rate == 0) {
+			yield break;
+		}
+
+		var waitTime = 60/rate;
+
+		while (true) {
+			yield return new WaitForSeconds(waitTime);
+			spawnCoinInRandomLocation(coin);
+		}
+	}
+
+	private void calculateCoinRates(float salary) {
+		var salaryPerWeek = salary/52.0f;
+		var salaryPerDay = salaryPerWeek/5.0f;
+		var penniesPerDay = salaryPerDay/pennyValue;
+
+		var penniesPerMinute = penniesPerDay*8.0f*60.0f;
+		setCoinRate(penny, penniesPerMinute);
+
+		Debug.Log("Pennies per minute = " + penniesPerMinute);
+
+		// TODO: Set this up for a variety of coins, not just pennies
+	}
+
+	private void resetCoinRates() {
+		setCoinRate(penny, 0f);
+		setCoinRate(nickel, 0f);
+		setCoinRate(dime, 0f);
+		setCoinRate(quarter, 0f);
+	}
+
+	private void setCoinRate(GameObject coin, float rate) {
+		coinRate[coin] = rate;
+	}
+
+	private void startSpawningAllCoinObjects() {
+		pennyCoroutine = StartCoroutine(spawnCoinAtRate(penny, coinRate[penny]));
+		nickelCoroutine = StartCoroutine(spawnCoinAtRate(nickel, coinRate[nickel]));
+		dimeCoroutine = StartCoroutine(spawnCoinAtRate(dime, coinRate[dime]));
+		quarterCoroutine = StartCoroutine(spawnCoinAtRate(quarter, coinRate[quarter]));
+	}
+
+	private void spawnCoinInRandomLocation(GameObject coin) {
+		var location = Vector3.zero;
+
+		var maxX = containingWidth/2.0f;
+		var minX = -1*maxX;
+
+		var maxZ = containingLength/2.0f;
+		var minZ = -1*maxZ;
+
+		// set x and z to random numbers within location bounds
+		location.x = Random.Range(minX, maxX);
+		location.z = Random.Range(minZ, maxZ);
+
+		spawnCoinAtLocation(coin, location);
+	}
+
+	private void spawnCoinAtLocation(GameObject coin, Vector3 location) {
+		// TODO: 
+		var newCoin = Instantiate(coin, location, Quaternion.identity) as GameObject;
+		newCoin.transform.parent = coinContainer.transform;
+	}
+
+	private void StopAllCoroutines() {
+
+		if (pennyCoroutine != null) {
+			StopCoroutine(pennyCoroutine);
+		}
+
+		if (nickelCoroutine != null) {
+			StopCoroutine(nickelCoroutine);
+		}
+
+		if (dimeCoroutine != null) {
+			StopCoroutine(dimeCoroutine);
+		}
+
+		if (quarterCoroutine != null) {
+			StopCoroutine(quarterCoroutine);
+		}
+	}
+
+	// Public Methods
+
+	public void restartDispensingWithSalary(float salary) {
+		stopDispensing();
+		salaryAmount = salary;
+		startDispensing();
+	}
+
+	public void startDispensing() {
+		calculateCoinRates(salaryAmount);
+		startSpawningAllCoinObjects();
+	}
+
+	public void stopDispensing() {
+		resetCoinRates();
+		StopAllCoroutines();
+	}
+		
+	public void removeAllCoins() {
+		Transform[] childTransforms = coinContainer.GetComponentsInChildren<Transform>();
+
+		foreach(Transform coin in childTransforms){
+			Destroy(coin.gameObject);
+		}
+	}
+}
